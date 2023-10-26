2d7e3cf1e1a2a00139ef807a5b23b747be16e024
Lucas Dutra <94666925+Lucasszsd@users.noreply.github.com>
2023-10-25 23:37:00 -0300

    Chamadas do banco

--- src/main/java/pr/edu/unicesumar/crud/Controller/PessoaController.java
+++ src/main/java/pr/edu/unicesumar/crud/Controller/PessoaController.java
@@ -21,41 +21,30 @@ public class PessoaController {
 
     }
     @GetMapping("/{id}")
-    public Pessoa getId (@PathVariable Long id){
-        return mock()
-                .stream()
-                .filter(pessoa -> pessoa.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+    public Pessoa getById (@PathVariable Long id){
+        return pessoaRepository.findById(id).orElse(null);
     }
     @GetMapping("/pesquisa")
     public List<Pessoa> getByNome(@RequestParam("search") String pesquisa){
-        return mock()
-                .stream()
-                .filter(pessoa -> pessoa.getNome().toLowerCase().contains(pesquisa.toLowerCase()))
-                .collect(Collectors.toList());
-    }
-    private List<Pessoa> mock() {
-        return Arrays.asList(new Pessoa(1L, "Lucas", "123"),
-        new Pessoa(2L, "Paulo", "456"),
-        new Pessoa(3L, "Pedro", "789"),
-        new Pessoa(4L, "Joao", "111"),
-        new Pessoa(5L, "Carlos", "222"),
-        new Pessoa(6L, "Maria", "333")
-        );
-    }
-    @PutMapping
-    public void create(@RequestBody Pessoa novaPessoa){
-       Pessoa pessoa = new Pessoa(1L,novaPessoa.getNome(),novaPessoa.getDocumento()
-        );
+        return null;
     }
     @PutMapping("/{id}")
-    public void update(@PathVariable Long id ,@RequestBody Pessoa editPessoa){
-      Pessoa pessoa =   new Pessoa(id, editPessoa.getNome(), editPessoa.getDocumento());
+    public Pessoa edit(@PathVariable Long id ,@RequestBody Pessoa editPessoa){
+      Pessoa pessoaDb = pessoaRepository.findById(id).orElse(new Pessoa());
+      pessoaDb.setId(editPessoa.getId());
+      pessoaDb.setNome(editPessoa.getNome());
+      pessoaDb.setDocumento(editPessoa.getDocumento());
+
+
+      return pessoaRepository.save(pessoaDb);
     }
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id){
-        System.out.println("DELETADO");
+        pessoaRepository.deleteById(id);
     }
 
+    @PostMapping
+    public Pessoa create(@RequestBody Pessoa nova){
+       return pessoaRepository.save(nova);
+    }
 }
--- src/main/java/pr/edu/unicesumar/crud/model/domain/Pessoa.java
+++ src/main/java/pr/edu/unicesumar/crud/model/domain/Pessoa.java
@@ -1,49 +1,20 @@
 package pr.edu.unicesumar.crud.model.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "ES_PESSOA")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Pessoa {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
     @Column(name = "doc", length = 14)
     private String documento;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public Pessoa(Long id, String nome, String documento) {
-        this.id = id;
-        this.nome = nome;
-        this.documento = documento;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public Pessoa(){
-    }
 }
